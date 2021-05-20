b8c99edfbae260d95972870fadf823101203dfb9
Pedro Pareschi <pedropareschi15@gmail.com>
2021-05-20 18:51:33 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -33,6 +34,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                 case 403:
                     this.handle403();
                     break;
+
+                case 422:
+                    this.handle422(errorObj);
+                    break
                 default:
                     this.handleDefaultError(errorObj);
             }
@@ -40,6 +45,8 @@ export class ErrorInterceptor implements HttpInterceptor {
             return Observable.throw(errorObj);
         }) as any;
     }
+
+    
     handle403(){
         this.storage.setLocalUser(null);
     }
@@ -68,6 +75,28 @@ export class ErrorInterceptor implements HttpInterceptor {
             ]
         });
         alert.present();
+    }
+
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    private listErrors(messages: FieldMessage[]): string {
+        let s : string = '';
+        for(var i = 0; i < messages.length;i++){
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 }
 
