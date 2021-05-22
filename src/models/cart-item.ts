242a20fbde61e0408d3538921e00be1c8e586fac
Pedro Pareschi <pedropareschi15@gmail.com>
2021-05-22 15:50:40 -0300

    Terminando as funcionalidades do carrinho

--- src/models/cart-item.ts
+++ src/models/cart-item.ts
@@ -1,6 +1,6 @@
 import { ProdutoDTO } from "./produto.dto";
 
-export interface CartItem{
+export interface CartItem {
     quantidade: number,
     produto: ProdutoDTO
 }
--- src/models/cart.ts
+++ src/models/cart.ts
@@ -1,5 +1,5 @@
-import { CartItem } from "./cart.item";
+import { CartItem } from "./cart-item";
 
-export interface Cart{
+export interface Cart {
     items: CartItem[]
 }
