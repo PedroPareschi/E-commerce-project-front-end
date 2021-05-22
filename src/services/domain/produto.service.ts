e511d28f03fcb7ab3fd6a0eef6ea50d1c30cca61
Pedro Pareschi <pedropareschi15@gmail.com>
2021-05-21 22:40:37 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -10,12 +10,21 @@ export class ProdutoService{
     constructor(public http: HttpClient){
     }
 
+    findById(produto_id: string){
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findByCategoria(categoria_id: string): Observable<ProdutoDTO[]>{
         return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
     }
 
     getSmallImageFromBucket(id: string): Observable<any>{
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType: 'blob'})
+    }
+
+    getImageFromBucket(id: string): Observable<any>{
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
         return this.http.get(url, {responseType: 'blob'})
     }
 }
